d7527f965bdf3e3b0ad0a16785d4f163882f11d6
kapilbhosale <kapils.07@gmail.com>
2014-07-07 18:16:59 +0530

    migration change

--- db/migrate/20140706145216_add_column_to_cutoff.rb
+++ db/migrate/20140706145216_add_column_to_cutoff.rb
@@ -1,5 +1,9 @@
 class AddColumnToCutoff < ActiveRecord::Migration
   def up
-  	add_column :cutoffs, :university, :string, :after => :branch_name
+  	execute "INSERT INTO cutoffs ('id', 'college_id', 'college_name', 'branch_id', 'branch_name', 'university', 'cast', 'rank_closed', 'marks_closed', 'created_at', 'updated_at')
+VALUES
+	(1, 1002, 'Government College of Engineering  Amravati', 100219110, 'Civil Engineering', 'Shivaji University', 'GOPENO', 6029, 132, NULL, NULL),
+	(3, 1002, 'Government College of Engineering  Amravati', 100219110, 'Civil Engineering', 'Shivaji University', 'GSTO', 20668, 99, NULL, NULL);
+"
   end
 end
