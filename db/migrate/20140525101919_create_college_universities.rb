e953cc00a4f4242b04ed50d166c1f5d5ea110667
kapilbhosale <kapils.07@gmail.com>
2014-07-07 15:40:59 +0530

    migration changes

--- db/migrate/20140525101919_create_college_universities.rb
+++ db/migrate/20140525101919_create_college_universities.rb
@@ -1,9 +1,5 @@
 class CreateCollegeUniversities < ActiveRecord::Migration
   def change
-    create_table :college_universities do |t|
-      t.integer :college_id, :null => false
-      t.string 	:university, :null => false	
-      t.timestamps
-    end
+   
   end
 end
